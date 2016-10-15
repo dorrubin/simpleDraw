5ba4b8924bbd8299f43b2e826fe55d9b6b211593
Dor Rubin <dor.rubin@gmail.com>
2016-10-15 17:20:56 -0400

    Initial Commit

---
+++ simpleDraw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleDraw
+//
+//  Created by Dor Rubin on 10/15/16.
+//  Copyright © 2016 Dor Rubin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
